1be873ad1cb7bd65d33b2692fd3aacabc4a4bbc4
razz1992 <kumarrajneesh75@yahoo.co.in>
2024-03-22 13:24:54 +0000

    commit from gcp

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!. First successfull app to gcp1';
+    return `Hello World!. First successfull app to gcp1. 
+    \n This line is changed in GCP editor`;
   }
 }
